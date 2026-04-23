92ad66652701388bab599fba3f6f0c95863f16f6
aminabelfakir-404 <am.belfakir@edu.umi.ac.ma>
2026-04-23 01:56:34 +0100

    Update hello.py
    ai_agent

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello from Errachidia!")
+user_quesion= "ما هو مستقبل الذكاء الاصطناعي في المغرب؟"
+def ai_agent(question): 
+response = f"أنا الوكيل الذكي، ردي على سؤالك '{question}' هو: المستقبل واعد جداً، خاصة مع شباب مدينة الرشيدية!"
+    return response
+print(ai_agent(user_question))
